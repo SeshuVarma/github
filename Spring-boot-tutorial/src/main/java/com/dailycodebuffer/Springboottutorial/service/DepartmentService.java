18d7da96fdb7ae63add45353f77cd13b5679e4f0
SeshuVarma <d.seshuvarma@gmail.com>
2023-04-24 17:59:29 -0700

    Commit SpringBoot

---
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboottutorial/service/DepartmentService.java
@@ -0,0 +1,20 @@
+package com.dailycodebuffer.Springboottutorial.service;
+
+import com.dailycodebuffer.Springboottutorial.entity.Department;
+import com.dailycodebuffer.Springboottutorial.error.DepartmentNotFoundException;
+
+import java.util.List;
+
+public interface DepartmentService {
+    public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
+
+    public Department fetchDepartmentById(Long departmentId) throws DepartmentNotFoundException;
+
+    public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartment(Long departmentId, Department department);
+
+    public Department fetchDepartmentByName(String departmentName);
+}
---
+++ Spring-boot-tutorial/src/test/java/com/dailycodebuffer/Springboottutorial/controller/DepartmentControllerTest.java
@@ -0,0 +1,76 @@
+package com.dailycodebuffer.Springboottutorial.controller;
+
+import com.dailycodebuffer.Springboottutorial.entity.Department;
+import com.dailycodebuffer.Springboottutorial.error.DepartmentNotFoundException;
+import com.dailycodebuffer.Springboottutorial.service.DepartmentService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcResultMatchersDsl;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(DepartmentController.class)
+class DepartmentControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private DepartmentService departmentService;
+
+    private Department department;
+
+    @BeforeEach
+    void setUp() {
+        department = Department.builder()
+                .departmentAddress("Ahmedabad")
+                .departmentCode("IT-06")
+                .departmentName("IT")
+                .departmentId(1L)
+                .build();
+    }
+
+    @Test
+    void saveDepartment() throws Exception {
+        Department inputDepartment = Department.builder()
+                .departmentAddress("Ahmedabad")
+                .departmentCode("IT-06")
+                .departmentName("IT")
+                .departmentId(1L)
+                .build();
+
+        Mockito.when(departmentService.saveDepartment(inputDepartment))
+                .thenReturn(department);
+        mockMvc.perform(MockMvcRequestBuilders.post("/departments")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "\t\"departmentName\":\"IT\",\n" +
+                        "\t\"departmentAddress\":\"Admedabad\",\n" +
+                        "\t\"departmentCode\":\"IT-06\"\n" +
+                        "}"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void fetchDepartmentById() throws Exception {
+//        Mockito.when(departmentService.fetchDepartmentById(1L))
+//                .thenReturn(department);
+//
+//        mockMvc.perform(get("/departments/1")
+//                .contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$.departmentName").
+//                        value(department.getDepartmentName()));
+    }
+}
